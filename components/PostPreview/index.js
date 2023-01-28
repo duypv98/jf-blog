ec6f34107837e4f9a26820cf5880d4bf1ebc281d
duypv <duypv98@gmail.com>
2023-01-28 15:33:55 +0700

    fix: font-size

--- components/PostPreview/index.js
+++ components/PostPreview/index.js
@@ -1,4 +1,5 @@
 import moment from "moment";
+import Link from "next/link";
 import "./style.scss";
 
 /**
@@ -9,13 +10,15 @@ const PostPreview = (props) => {
   const { post } = props;
 
   return <div className="post-preview">
-    <a href={`/${post.slug}`}>
-      <h2 className="post-title">{post.title}</h2>
-      <h3 className="post-subtitle dot-2" dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-      <p className="post-meta">
-        {moment(new Date(post.createdAt)).format("[on] MMMM DD, YYYY")}
-      </p>
-    </a>
+    <Link href={`/${post.slug}`} passHref>
+      <a>
+        <h2 className="post-title">{post.title}</h2>
+        <h3 className="post-subtitle dot-2" dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+        <p className="post-meta">
+          {moment(new Date(post.createdAt)).format("[on] MMMM DD, YYYY")}
+        </p>
+      </a>
+    </Link>
   </div>
 }
 

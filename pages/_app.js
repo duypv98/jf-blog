47cb7784c93a91c5422d372e9e9b56318d9d62f4
duypv <duypv98@gmail.com>
2023-01-28 11:39:02 +0700

    fix: scroll & ssr

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import { SSRProvider } from "react-bootstrap";
 import { Provider } from "react-redux";
 import { store } from "../app/store";
 import { CMSAuthProvider } from "../context/CMSAuthContext";
@@ -7,12 +8,14 @@ import '../styles/_global.scss';
 const ToastifyConfig = dynamic(() => import("../components/ToastifyConfig"), { ssr: false });
 
 function MyApp({ Component, pageProps }) {
-  return <CMSAuthProvider>
-    <Provider store={store}>
-      <Component {...pageProps} />
-      <ToastifyConfig />
-    </Provider>
-  </CMSAuthProvider>
+  return <SSRProvider>
+    <CMSAuthProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />
+        <ToastifyConfig />
+      </Provider>
+    </CMSAuthProvider>
+  </SSRProvider>
 }
 
 export default MyApp
